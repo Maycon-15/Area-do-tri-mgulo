33faa5975296987d562179257a613fe06c694c9f
Maycon-15 <78987309+Maycon-15@users.noreply.github.com>
2021-03-21 22:53:29 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Area_Triângulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             string entrada1;
+            string entrada2;
+            double Area,Base, Altura;
+
+            Console.WriteLine("Digite abaixo as medidas do triangulo, que serão calculadas");
+            Console.Write("Base: ");
+            entrada1 = Console.ReadLine();
+            Base= Convert.ToDouble(entrada1);
+            Console.Write("Altura: ");
+            entrada2 = Console.ReadLine();
+            Altura= Convert.ToDouble(entrada2);
+            
+            Area = Base*Altura/2;
+            
+            Console.ForegroundColor=ConsoleColor.DarkBlue;
+            Console.WriteLine("\n---Area---");
+            Console .ResetColor ();
+            Console.WriteLine($"A área é: {Area}");
+            Console.WriteLine("----------");
+        }
+    }
+}
